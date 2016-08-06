d478a4b24f01c3ad3be0e8ba12d687ca58951afd
nischal kumar <nischal.k@pw-c02mxrwjfh00.fkipl.flipkart.com>
2016-08-06 14:26:52 +0530

    Initial Commit

---
+++ EMOJIFIER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMOJIFIER
+//
+//  Created by nischal kumar on 06/08/16.
+//  Copyright © 2016 nischal kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
